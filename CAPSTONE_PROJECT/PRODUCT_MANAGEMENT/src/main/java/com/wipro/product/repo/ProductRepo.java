99ebfa67f7da578e03538382ef480870b8c41448
edigamurali <edigamurali35@gmail.com>
2025-09-01 11:51:57 +0530

    commit

--- CAPSTONE_PROJECT/PRODUCT_MANAGEMENT/src/main/java/com/wipro/product/repo/ProductRepo.java
+++
@@ -1,7 +0,0 @@
-package com.wipro.product.repo;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.wipro.product.entity.Product;
-
-public interface ProductRepo extends JpaRepository<Product, Long> {
-}
